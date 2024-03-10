91e636d591a30b4ad72241daaa33aa8a5b4d0e48
Michael <gt.gluck@gmail.com>
2024-03-10 16:50:59 +0200

    fix: all

---
+++ back/app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/user", require("./routes/users"));
+
+module.exports = app;
