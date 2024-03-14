e4858034c1bf94160b697853e23136e9066613db
Michael <gt.gluck@gmail.com>
2024-03-14 12:34:30 +0200

    add: aliases

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@common': '/src/common',
+      '@pages': '/src/pages',
+      '@utils': '/src/utils',
+      '@services': '/src/services'
+    }
+  }
 })
